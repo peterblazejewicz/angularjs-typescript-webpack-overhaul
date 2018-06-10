e260f2bf9d1b12c587f34426afa6bececc6fe3c2
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-06-10 23:53:39 +0200

    Webpack 4 TS basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    context: __dirname,
+};
